d1bb2353fd4285c17e2549b92113703e5ae1a708
Yash Sonwane <yashsonwane@gmail.com>
2022-02-16 10:49:34 +0530

    chamged credential

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import enum
 
 
 class Credentials(enum.Enum):
-    UserName = "523731"
-    PassWord = "Plokij*898"
-    SecretKey = "1A8EeQpgdfXmWZIp5eCXsnyFWWiWc2oqmU2vSVJuGwSjsSfJ76U3AMFdXUpfIl9o"
-    AppId = "yahsG2Di5g"
+    UserName = "YourUserName"
+    PassWord = "YourPassword"
+    SecretKey = "YourSecretKey"
+    AppId = "YourAapID"
